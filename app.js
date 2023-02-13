1618e935667de1988a806ec9632a4e9d98eef2bd
bxngtannn <dxnt3dream@gmail.com>
2023-02-13 10:18:10 -0300

    agregado app y cambios para que funcione

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const port = 3030;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve('./views/home.html')));
+
+app.get('/login', (req, res) => res.sendFile(path.resolve('./views/login.html')))
+
+app.get('/register', (req, res) => res.sendFile(path.resolve('./views/register.html')))
+
+app.get('/productCart', (req, res) => res.sendFile(path.resolve('./views/productCart.html')))
+
+app.get('/productDetail', (req, res) => res.sendFile(path.resolve('./views/productDetail.html')))
+
+app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}!`));
