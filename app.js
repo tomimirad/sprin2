8042856aa91f0756a15d5a7e225ab56df175e2c3
bxngtannn <dxnt3dream@gmail.com>
2023-02-18 11:40:54 -0300

    nuevo header mejorado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve('./views/home.html')));
 
-app.get('/headerfooter', (req, res) => res.sendFile(path.resolve('./views/HeaderFooter.html')));
+app.get('/header', (req, res) => res.sendFile(path.resolve('./views/header.html')));
 
 app.get('/login', (req, res) => res.sendFile(path.resolve('./views/login.html')))
 
